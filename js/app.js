66ab96992e6fad1048b728a0d16f74c1d0e0276c
Federico-Maiocchi <maiocchifederico@gmail.com>
2023-10-06 15:21:17 +0200

    ADD: console.log fizzuzz fizz buzz

--- js/app.js
+++ js/app.js
@@ -10,7 +10,31 @@
 
 for (let i = 0; i < 100; i++) {
     const n = i + 1;
-    console.log(n)
+    // console.log(n)
+    
+    const mult3 = n % 3;
+    
+    const mult5 = n % 5;
+
+    
+
+    // const 
+
+    if (mult3 === 0 && mult5 === 0) {
+        console.log('fizzbuzz');
+    }
+
+    else if (mult3 === 0) {
+        console.log('fizz');
+    }
+
+    else if (mult5 === 0) {
+        console.log('buzz');
+    }
+
+    else {
+        console.log(n)
+    }
     
     
 }
