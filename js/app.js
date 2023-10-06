617007793c4b79b39d1ec08ebbe08f026906ab6d
Federico-Maiocchi <maiocchifederico@gmail.com>
2023-10-06 16:21:41 +0200

    ADD: bonus creation obj

--- js/app.js
+++ js/app.js
@@ -8,6 +8,9 @@
 //     - verificare della condizione
 //     - eseguire tutto il codice delle graffe
 
+const gridDOMElement = document.querySelector('.grid')
+console.log(gridDOMElement)
+
 for (let i = 0; i < 100; i++) {
     const n = i + 1;
     // console.log(n)
@@ -16,22 +19,33 @@ for (let i = 0; i < 100; i++) {
     
     const mult5 = n % 5;
 
-    
+    // Aggiungi Stringa
+    // const boxString = '<div class="box">' + n + '</div>' 
+    // gridDOMElement.innerHTML += boxString
 
-    // const 
+    // Creazione oggetti
+    const boxDomElement = document.createElement('div');
+    boxDomElement.classList.add('box')
+    boxDomElement.innerHTML = n
 
+    gridDOMElement.append(boxDomElement)
+
+    // multipli di 3 che coincidono con multipli di 5
     if (mult3 === 0 && mult5 === 0) {
+    
         console.log('fizzbuzz');
     }
-
+    // multipli di 3
     else if (mult3 === 0) {
+        
         console.log('fizz');
     }
-
+    // multipli di 5
     else if (mult5 === 0) {
+        
         console.log('buzz');
     }
-
+    // numeri normali
     else {
         console.log(n)
     }
