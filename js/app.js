bb8e8f719a8361c61c03a25f4f2ffdf94d9b73ac
Federico-Maiocchi <maiocchifederico@gmail.com>
2023-10-06 14:38:26 +0200

    ADD: for num i < 100

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log('hello')
+// console.log('hello')
+
+// - Genare un ciclo di numeri da 1 100 in modo crescente
+//     - dichiarare l'indice
+//     - verificare le condizioni
+//     - eseguire il codice 
+//     - eseguire istruzioni incremento
+//     - verificare della condizione
+//     - eseguire tutto il codice delle graffe
+
+for (let i = 0; i < 100; i++) {
+    const n = i + 1;
+    console.log(n)
+    
+    
+}
